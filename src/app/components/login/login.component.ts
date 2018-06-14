0885836d5e375eaba046385b76d8511ee023b71c
david <david11sone@gmail.com>
2018-06-14 22:47:57 +0530

    dsadas

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,26 +7,31 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { 
-     
-  }
-  isLoading = false;
-  userOBJ = <any>{};    
-  public token:string;
-//  private authService:AuthenticateService;
+  constructor(private router: Router) {
 
-  ngOnInit():void{
+  }
+  private isLoading = false;
+  private userOBJ = <any>{};
+  public token: string;
+  //  private authService:AuthenticateService;
+
+  ngOnInit(): void {
     this.token = "db2961f3f56bbea01667312e4b6cc6f3d1aa536e"
   }
 
-  dologin(form: any): void{
-    localStorage.setItem('currentUser', JSON.stringify({ username: this.userOBJ.username, token: this.token }));
-    this.router.navigate(['/dashboard']);
-    
-    
+  dologin(form: any): void {
+    this.isLoading = true;
+    setTimeout(() => {
+      localStorage.setItem('currentUser', JSON.stringify({ username: this.userOBJ.username, token: this.token }));
+      this.router.navigate(['/dashboard']);
+      this.isLoading = false;
+    }, 3000);
+
+
+
 
     //this.authService.login(this.userOBJ.username,this.userOBJ.password);
-    
+
   }
 
 
