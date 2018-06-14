fcee13d9d6b49ddb6ac715986705a1b552be51a5
david <david11sone@gmail.com>
2018-06-14 23:27:03 +0530

    asdasd

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  public isLoading = false;
+  
+  constructor() { 
+    localStorage.removeItem('currentUser');
+  }
 
   ngOnInit() {
+    
   }
+
+
+  
 
 }
